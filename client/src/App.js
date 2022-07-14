6a7fd3aadfb3f14cb745950c99618fc071592f41
moisesdurandelrazo <moises6737@gmail.com>
2022-07-13 19:37:31 -0500

    Adding navbar to mainscream

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Pokemons from "./components/Pokemons";
 import PokemonsDetail from "./components/PokemonDetail";
-import CreatePokemon from "./components/CreatePokemon"
-import PokeHome from "./components/PokeHome"
+import CreatePokemon from "./components/CreatePokemon";
+import PokeHome from "./components/PokeHome";
+import Nav from "./components/Nav/Nav";
 // import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Switch>
         <Route exact path="/" component={PokeHome} />
         <Route exact path="/pokemons" component={Pokemons} />
-        <Route path="/pokemons/:id" component={PokemonsDetail}/>
-        <Route exact path="/create" component={CreatePokemon}/>
+        <Route path="/pokemons/:id" component={PokemonsDetail} />
+        <Route exact path="/create" component={CreatePokemon} />
       </Switch>
     </div>
   );
