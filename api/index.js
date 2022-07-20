b6adfa6c228916a516a9d5f25c41fc1fcca446d8
moisesdurandelrazo <moises6737@gmail.com>
2022-07-20 02:11:53 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -17,6 +17,18 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT ? process.env.PORT : 3001;
