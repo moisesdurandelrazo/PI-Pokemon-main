a4b109a8ae7d6e5a71a8856f574140b4791c5f32
moisesdurandelrazo <moises6737@gmail.com>
2022-07-20 02:33:11 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -17,22 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT ? process.env.PORT : 3001;
 console.log({ port });
+
+import cors from "cors";
+
+app.use(cors());
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
