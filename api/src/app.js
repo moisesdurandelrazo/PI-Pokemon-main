05b86934e48083ee067e25858a5b1df0e5bbacd0
moisesdurandelrazo <moises6737@gmail.com>
2025-01-13 02:12:20 -0600

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -5,27 +5,35 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 require("dotenv").config();
 const routes = require("./routes/index.js");
+const cors = require("cors");
 
 require("./db.js");
 
 const server = express();
 
 server.name = "API";
+const allowedOrigins = [
+  process.env.CLIENT_URL_DEV,
+  process.env.CLIENT_URL_PROD,
+];
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+};
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(cors(corsOptions));
 
 server.use("/", routes);
 
