5a27baf8614bc3aaf76ca6ff9d0898c5e05d4b18
moisesdurandelrazo <moises6737@gmail.com>
2025-01-15 11:34:39 -0600

    css pokemon

--- client/src/components/Pokemon/Pokemon.jsx
+++ client/src/components/Pokemon/Pokemon.jsx
@@ -28,7 +28,7 @@ const Pokemon = ({ name, img, types, id }) => {
         minHeight: 400,
         borderRadius: 6,
         boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-        margin: "20px auto",
+        margin: "10px auto",
         background: "linear-gradient(0deg, #ffffff , #f0f0f0)",
         display: "flex",
         flexDirection: "column",
@@ -38,21 +38,26 @@ const Pokemon = ({ name, img, types, id }) => {
       }}
     >
       <Link to={`/pokemons/${id}`} style={{ textDecoration: "none" }}>
-        <CardMedia
-          component="img"
-          height="180"
-          image={img || "https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif"}
-          alt={name}
-          sx={{
-            objectFit: "contain",
-            margin: "10px auto",
-            transition: "transform 0.2s",
-            "&:hover": {
-              transform: "scale(1.1)",
-            },
-          }}
-        />
-        <CardContent>
+        <Box sx={{ background: "#eeeeee" }}>
+          <CardMedia
+            component="img"
+            height="180"
+            image={
+              img || "https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif"
+            }
+            alt={name}
+            sx={{
+              objectFit: "contain",
+              margin: "5px auto",
+              transition: "transform 0.2s",
+              background: "#eeeeee",
+              "&:hover": {
+                transform: "scale(1.1)",
+              },
+            }}
+          />
+        </Box>
+        <CardContent sx={{ padding: "0px" }}>
           <Typography
             variant="h6"
             component="div"
@@ -60,6 +65,8 @@ const Pokemon = ({ name, img, types, id }) => {
               fontWeight: "bold",
               textAlign: "center",
               color: "#333",
+              margin: "0 0 0 0",
+              padding: 1,
             }}
           >
             {name.toUpperCase()}
@@ -70,6 +77,7 @@ const Pokemon = ({ name, img, types, id }) => {
               textAlign: "center",
               color: "#555",
               marginBottom: 1,
+              padding: 0,
             }}
           >
             N.º {id.toString().padStart(3, "0")}
@@ -80,6 +88,7 @@ const Pokemon = ({ name, img, types, id }) => {
               justifyContent: "center",
               gap: 1,
               flexWrap: "wrap",
+              padding: 1,
             }}
           >
             {types &&
@@ -93,6 +102,7 @@ const Pokemon = ({ name, img, types, id }) => {
                     fontWeight: "bold",
                     textTransform: "capitalize",
                     fontSize: "12px",
+                    padding: "2px 8px",
                   }}
                 />
               ))}
@@ -100,35 +110,39 @@ const Pokemon = ({ name, img, types, id }) => {
         </CardContent>
       </Link>
       {isFavorite ? (
-        <img style={{
-          position: "absolute",
-          top: "10px",
-          right: "10px",
-          width: "30px",
-          height: "30px",
-        }} src={poke} alt="Favorite" />
+        <img
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            width: "30px",
+            height: "30px",
+          }}
+          src={poke}
+          alt="Favorite"
+        />
       ) : (
         <Box
-        onClick={() => dispatch(addFav( id ))}
-        sx={{
-          position: "absolute",
-          bottom: 10,
-          right: "50%",
-          transform: "translateX(50%)",
-          cursor: "pointer",
-          padding: "8px 8px",
-          borderRadius: "20px",
-          backgroundColor: "#3f51b5",
-          color: "white",
-          fontWeight: "bold",
-          fontSize: "10px",
-          transition: "background-color 0.3s",
-          height: "content",
-          width: "fit-content",
-          "&:hover": {
-            backgroundColor: "#2c387e",
-          },
-        }}
+          onClick={() => dispatch(addFav(id))}
+          sx={{
+            position: "absolute",
+            bottom: 10,
+            right: "50%",
+            transform: "translateX(50%)",
+            cursor: "pointer",
+            padding: "8px 8px",
+            borderRadius: "20px",
+            backgroundColor: "#3f51b5",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "10px",
+            transition: "background-color 0.3s",
+            height: "content",
+            width: "fit-content",
+            "&:hover": {
+              backgroundColor: "#2c387e",
+            },
+          }}
         >
           Agregar a favoritos
         </Box>
@@ -150,25 +164,24 @@ Pokemon.propTypes = {
 
 const getTypeColor = (type) => {
   const typeColors = {
-    grass: "rgb(120,200,80)",
-    fire: "rgb(240,80,48)",
-    water: "rgb(56,153,248)",
-    poison: "rgb(176,88,160)",
-    flying: "#BAAAFF",
-    normal: "rgb(168,160,144)",
-    bug: "rgb(168,184,32)",
-    electric: "rgb(248,208,48)",
-    ground: "rgb(234,214,164)",
-    psychic: "rgb(248,112,160)",
-    rock: "rgb(184,160,88)",
-    fighting: "rgb(160,80,56)",
-    ice: "rgb(88,200,224)",
-    ghost: "rgb(96,96,176)",
-    steel: "rgb(168,168,192)",
-    dragon: "#AB82FF",
-    dark: "#705848",
-    fairy: "#FFB0FF",
-    shadow: "#A9A9A9",
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
     unknown: "transparent",
   };
   return typeColors[type.toLowerCase()] || "#ddd";
